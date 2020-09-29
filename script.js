6ea59608466318ef9888f6bd2097e9b269022dc6
bmukha <47007157+bmukha@users.noreply.github.com>
2020-09-29 13:50:38 +0300

    works, but not perfect. will improve later

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
   let outcome = "";
-  let color = 'magenta';
+  let color = "gray";
   if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
     outcome = "draw";
   } else if (
@@ -23,54 +23,37 @@ function playRound(playerSelection, computerSelection) {
   ) {
     playerScore++;
     outcome = "won";
-    color = 'green';
+    color = "green";
   } else {
     computerScore++;
     outcome = "lost";
-    color = 'red';
+    color = "red";
   }
   resultsString.innerHTML = `<p>You chose:</p>
-    <img src="./img/${playerSelection.toLowerCase()}.jpg" alt=${playerSelection.toLowerCase()} width="150px" height="150px" />
+    <img src="./img/${playerSelection.toLowerCase()}.png" alt=${playerSelection.toLowerCase()} width="150px" height="150px" />
     <p>Computer chose:</p>
-    <img src="./img/${computerSelection.toLowerCase()}.jpg" alt=${computerSelection.toLowerCase()} width="150px" height="150px" />
+    <img src="./img/${computerSelection.toLowerCase()}.png" alt=${computerSelection.toLowerCase()} width="150px" height="150px" />
     <p style="background-color:${color};">You ${outcome} the round!</p>`;
   resultsDisplay.innerText = `${playerScore}:${computerScore}`;
-}
 
-// function game() {
-//   let userScore = 0;
-//   let computerScore = 0;
-//   for (let i = 0; i < 5; i++) {
-//     let userInput = prompt("Choose between Rock, Paper and Scissors", "");
-//     let resultOfTheRound = playRound(userInput, computerPlay());
-//     if (resultOfTheRound[4] === "W") {
-//       userScore++;
-//     }
-//     if (resultOfTheRound[4] === "L") {
-//       computerScore++;
-//     }
-//     console.log(resultOfTheRound);
-//     console.log(`Score: ${userScore}:${computerScore}`);
-//   }
-//   let resultOfTheGame = "";
-//   if (userScore > computerScore) {
-//     resultOfTheGame = "You Won the game!";
-//   } else if (userScore < computerScore) {
-//     resultOfTheGame = "You Lose the game!";
-//   } else {
-//     resultOfTheGame = "It's a Draw!";
-//   }
-//   console.log(`${resultOfTheGame} Final score: ${userScore}:${computerScore}`);
-// }
+  if (computerScore == 5 || playerScore == 5) {
+    setTimeout(() => {
+      resultsString.innerHTML = `<p style="background-color:${color};"> ${
+        computerScore == 5 ? "Commiserations" : "Congratulations"
+      }! You ${outcome} the match!</p>`;
+      computerScore = 0;
+      playerScore = 0;
+      resultsDisplay.innerText = `${playerScore}:${computerScore}`;
+    }, 2000);
+
+    setTimeout(() => {
+      resultsString.innerHTML = `<p>Let's play again!</p>`;
+    }, 4000);
+  }
+}
 
 const options = document.querySelectorAll(".options");
 console.log(options);
-// options.forEach((element) =>
-//   addEventListener("click", (e) => {
-//     alert(`you clicked ${element.attributes[0].value}`);
-//     e.stopPropagation();
-//   })
-// );
 
 options[0].addEventListener("click", (e) =>
   playRound(options[0].attributes[0].value, computerPlay())
